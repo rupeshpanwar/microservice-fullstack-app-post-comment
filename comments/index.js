ad9d7b12de2aa264c5304fa15d98b97e8885f9f3
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-11 06:18:02 +0530

    Comment Service- listen for Comment moderation and emit event

--- comments/index.js
+++ comments/index.js
@@ -43,6 +43,27 @@ app.post('/posts/:id/comments', async (req, res) => {
 app.post('/events', (req, res) => {
     console.log('Event created', req.body.type)
 
+    const { type, data } = req.body
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data
+
+        const comments = commentsByPostId[postId]
+
+        const comment = comments.find(comment => {
+            return comment.id === id
+        })
+
+        comment.status = status
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id, status, content, postId
+            }
+        })
+    }
+
     res.send({})
 })
 
